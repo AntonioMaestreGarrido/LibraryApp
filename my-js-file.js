29779f247e00e7a4587ecbb108a3c8d9a6eadfb2
Antonio <antoniomaestregarrido.com>
2021-11-01 15:07:15 +0100

    .

--- my-js-file.js
+++ my-js-file.js
@@ -1,34 +1,35 @@
 
 const myLibrary = [];
 
-for (let i = 0; i < 2; i++) { add() }
+
 
 function add() {
-    
+
     var title = prompt("title");
     var autor = prompt("autor");
     var pages = prompt("pages?");
     while (read != "y" && read != "n") {
         var read = prompt("Leido?")
         console.log(read)
-    } 
-    if(read="y"){read=true}else{read=false}
-   
+    }
+    if (read = "y") { read = true } else { read = false }
+
     var b = new book(title, autor, pages, read)
 
     myLibrary.push(b)
 
 }
-console.table(myLibrary)
 
 
 
 
 
-var primero = new book("Dune", "Frank Herbet", "899", true)
 
 
-console.log(primero.list())
+
+
+
+
 
 function book(title, autor, pages, read) {
     this.title = title
