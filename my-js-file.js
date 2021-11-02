e3fb4f456268600c9cd01382a8731389e7045645
Antonio <antoniomaestregarrido.com>
2021-11-02 07:17:23 +0100

    .

--- my-js-file.js
+++ my-js-file.js
@@ -1,23 +1,95 @@
 
 const myLibrary = [];
+const formu = document.getElementById("addBook")
+setListeners()
+//dummy()
 
 
 
-function add() {
+function dummy(){
+    var a = new book("Dune", "Frank Herbert", "333", true);
+    var b = new book("Game of thrones", "George Martin", "983", true);
+    var c = new book("Mort", "Terry Pratchett", "350", true);
+    var d = new book("Dune", "Frank Herbert", "333", true);
+    var e = new book("Game of thrones", "George Martin", "983", true);
+    var f = new book("Mort", "Terry Pratchett", "350", true);
+    myLibrary.push(a)
+    myLibrary.push(b)
+    myLibrary.push(c)
+    myLibrary.push(d)
+    myLibrary.push(e)
+    myLibrary.push(f)
+   // makeTable(myLibrary)
+}
 
-    var title = prompt("title");
-    var autor = prompt("autor");
-    var pages = prompt("pages?");
-    while (read != "y" && read != "n") {
-        var read = prompt("Leido?")
-        console.log(read)
+function setListeners() {
+    document.querySelector("#submit").addEventListener("click", askForBook)
+    document.querySelector("#abreForm").addEventListener("click", openform)
+    document.querySelector("#close").addEventListener("click", closeForm)
+    //addBook()
+    
+
+}
+function makeTable(array) {
+    var table = document.createElement('table');
+    var keys = Object.keys( myLibrary[0]);
+    //primera linea con los nombres
+    var row = document.createElement('tr');
+    for (var j=0;j<keys.length;j++){
+        var cell = document.createElement('td');
+        cell.textContent = (keys[j]);
+        
+            row.appendChild(cell);
     }
-    if (read = "y") { read = true } else { read = false }
+    table.appendChild(row);
+
+    for (var i = 0; i < array.length; i++) {
+        
+        var row = document.createElement('tr');
+        for (var j=0;j<keys.length;j++)
+         {
+           
+            
+           // console.log(keys[j]+"="+myLibrary[i][keys[j]])
+            var cell = document.createElement('td');
+            cell.textContent = myLibrary[i][keys[j]];
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+    //console.log(table)
+    document.querySelector("#booklist").appendChild(table)
+   
+    
+}
+function closeForm(){
+   formu.style.display = 'none';
+   makeTable(myLibrary);
+}
+function openform(){
+       formu.style.display = 'flex';
+}
+function askForBook() {
+    addBook()
+    
+    
+}
+
+function addBook() {
+
+    
+    var title = formu.elements["title"].value
+    var autor = formu.elements[1].value
+    var pages = formu.elements[2].value
+    var read = formu.elements["read"].value
+
+
+    if (read === "y") { read = true } else { read = false }
 
     var b = new book(title, autor, pages, read)
 
     myLibrary.push(b)
-
+    console.table(myLibrary)
 }
 
 
@@ -37,6 +109,6 @@ function book(title, autor, pages, read) {
     this.pages = pages
     this.read = read
 
-    this.list = function () { return `${title} by ${autor}, ${pages} long` }
+    
 
 }
